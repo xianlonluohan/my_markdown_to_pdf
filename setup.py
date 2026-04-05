bad368054ea6fd05bad4e3136106ae4a5fa501c9
xianlonluohan <2047669296@qq.com>
2026-04-05 11:58:06 +0800

    再次修改脚本使用方式

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="emakefun_markdown_to_pdf",
-    version="1.0.0",
+    version="1.0.1",   # 升级版本号
     description="Convert Markdown documents to high-quality PDFs with one click, supporting single files, batch directories, watermarks, and automatic ZIP compression",
     author="DiLuKe",
     author_email="chenliang@null-lab.com",
@@ -10,10 +10,12 @@ setup(
     maintainer_email="chenliang@null-lab.com",
     url="https://github.com/xianlonluohan/my_markdown_to_pdf",
     keywords="markdown pdf converter playwright",
-    py_modules=["emakefun_markdown_to_pdf"],   
+    py_modules=["emakefun_markdown_to_pdf"],
+    package_dir={"": "."},          # 明确根目录
     package_data={
-        "": ["markdown.css"],                 
+        "": ["markdown.css"],
     },
+    include_package_data=True,
     install_requires=[
         "beautifulsoup4",
         "markdown",
@@ -25,10 +27,9 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "emakefun_markdown_to_pdf = emakefun_markdown_to_pdf:main",   
+            "emakefun_markdown_to_pdf = emakefun_markdown_to_pdf:main",
         ],
     },
     python_requires=">=3.7",
-    include_package_data=True,
     zip_safe=False,
 )
