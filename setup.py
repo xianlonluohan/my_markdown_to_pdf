26dc18061bfece7b8eaaa193e2863b3ae0d1aef6
xianlonluohan <2047669296@qq.com>
2026-04-05 12:37:05 +0800

    再次修改脚本使用方式

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="emakefun_markdown_to_pdf",
-    version="1.0.1",   # 升级版本号
+    version="1.0.2",   # 版本号可以升级一下
     description="Convert Markdown documents to high-quality PDFs with one click, supporting single files, batch directories, watermarks, and automatic ZIP compression",
     author="DiLuKe",
     author_email="chenliang@null-lab.com",
@@ -10,10 +10,9 @@ setup(
     maintainer_email="chenliang@null-lab.com",
     url="https://github.com/xianlonluohan/my_markdown_to_pdf",
     keywords="markdown pdf converter playwright",
-    py_modules=["emakefun_markdown_to_pdf"],
-    package_dir={"": "."},          # 明确根目录
+    packages=find_packages(),           # 自动找到 emakefun_markdown_to_pdf 包
     package_data={
-        "": ["markdown.css"],
+        "emakefun_markdown_to_pdf": ["markdown.css"],   # 明确指定包内的数据文件
     },
     include_package_data=True,
     install_requires=[
@@ -27,6 +26,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
+            # 命令名 = 包名:函数名
             "emakefun_markdown_to_pdf = emakefun_markdown_to_pdf:main",
         ],
     },
